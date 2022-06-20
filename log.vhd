a8ade0193257329a13d45133850914ba5bc06580
Michał Kruszewski <mkru@protonmail.com>
2022-06-20 09:54:33 +0200

    Add option to log to file.

--- log.vhd
+++ log.vhd
@@ -3,7 +3,7 @@
 -- Copyright (c) 2022 Michał Kruszewski
 
 library std;
-   use std.textio.all;
+   use std.textio;
 
 -- Log package implements simple logging mechanism.
 -- The one you have always wanted to use.
@@ -20,18 +20,9 @@ package log is
       separator     : string(1 to 3);
    end record;
 
-   constant DEFAULT_CONFIG : t_config := (
-      level         => INFO,
-      show_level    => true,
-      time_unit     => ns,
-      show_sim_time => true,
-      prefix        => (others => nul),
-      separator     => ": " & nul
-   );
-
    type t_logger is protected
-      impure function config return t_config;
       procedure set_config(c : t_config);
+      procedure set_output(filepath : string);
 
       procedure trace(msg : string);
       procedure debug(msg : string);
@@ -42,13 +33,22 @@ package log is
 
    shared variable logger : t_logger;
 
-   procedure set_level(l : t_level);
+   procedure set_config(cfg : t_config);
 
    procedure trace(msg : string);
    procedure debug(msg : string);
    procedure info(msg : string);
    procedure warn(msg : string);
    procedure error(msg : string);
+
+   function config(
+      level         : t_level := INFO;
+      show_level    : boolean := true;
+      time_unit     : time := ns;
+      show_sim_time : boolean := true;
+      prefix        : string(1 to 32) := (others => nul);
+      separator     : string(1 to 3) := ": " & nul
+   ) return t_config;
 
 end package;
 
@@ -62,15 +62,23 @@ package body log is
 
    type t_logger is protected body
 
-      variable cfg : t_config := DEFAULT_CONFIG;
+      variable cfg : t_config := config;
 
-      impure function config return t_config is begin return cfg; end function;
       procedure set_config(c : t_config) is begin cfg := c; end procedure;
+
+      file output : textio.text;
+      variable output_set : boolean := false;
+
+      procedure set_output(filepath : string) is
+      begin
+         textio.file_open(output, filepath, write_mode);
+         output_set := true;
+      end procedure;
 
       procedure log(lvl : t_level; msg : string) is
          constant MAX_TIME_LEN : positive := 32;
          variable time : string(1 to MAX_TIME_LEN);
-         variable time_line : line;
+         variable time_line : textio.line;
 
          procedure trim_time(t : inout string) is
          begin
@@ -82,14 +90,22 @@ package body log is
          if lvl < cfg.level then return; end if;
 
          if cfg.show_sim_time then
-            write(time_line, now, left, MAX_TIME_LEN, cfg.time_unit);
+            textio.write(time_line, now, textio.left, MAX_TIME_LEN, cfg.time_unit);
             time := time_line.all;
             trim_time(time);
          end if;
 
-         write(output, t_level'image(lvl) & cfg.separator & time & cfg.separator &  msg & LF);
-      end procedure;
+         if output_set then
+            textio.write(
+               output, cfg.prefix & t_level'image(lvl) & cfg.separator & time & cfg.separator & msg & LF
+            );
+         else
+            textio.write(
+               textio.output, cfg.prefix & t_level'image(lvl) & cfg.separator & time & cfg.separator & msg & LF
+            );
+         end if;
 
+      end procedure;
 
       procedure trace(msg : string) is begin log(TRACE, msg); end procedure;
       procedure debug(msg : string) is begin log(DEBUG, msg); end procedure;
@@ -99,12 +115,25 @@ package body log is
 
    end protected body;
 
-   procedure set_level(l : t_level) is
-      variable c : t_config;
+   procedure set_config(cfg : t_config) is begin logger.set_config(cfg); end procedure;
+
+   function config(
+      level         : t_level := INFO;
+      show_level    : boolean := true;
+      time_unit     : time := ns;
+      show_sim_time : boolean := true;
+      prefix        : string(1 to 32) := (others => nul);
+      separator     : string(1 to 3) := ": " & nul
+   ) return t_config is
+      variable cfg : t_config;
    begin
-      c := logger.config;
-      c.level := l;
-      logger.set_config(c);
-   end procedure;
+      cfg.level := level;
+      cfg.show_level := show_level;
+      cfg.time_unit := time_unit;
+      cfg.show_sim_time := show_sim_time;
+      cfg.prefix := prefix;
+      cfg.separator := separator;
+      return cfg;
+   end function;
 
 end package body;
--- test.vhd
+++ test.vhd
@@ -9,18 +9,33 @@ entity test is
 end entity;
 
 architecture tb of test is
+
 begin
    main : process is
+      variable l : log.t_logger;
    begin
       wait for 7.5 ns;
 
-      log.set_level(log.TRACE);
+      log.set_config(log.config(log.TRACE));
 
       log.trace("TRACE");
       log.debug("DEBUG");
       log.info("INFO");
       log.warn("WARN");
-      log.error("ERROR");
+      log.error("ERROR" & LF);
+
+      l.set_config(
+         log.config(
+            level => log.TRACE,
+            prefix => ("prefix: ", others => nul)
+         )
+      );
+      l.set_output("/tmp/vhdl/vhdl-log");
+      l.trace("TRACE");
+      l.debug("DEBUG");
+      l.info("INFO");
+      l.warn("WARN");
+      l.error("ERROR");
 
       std.env.finish;
    end process;
